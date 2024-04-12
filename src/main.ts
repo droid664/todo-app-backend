76bb69a8a3158d7fccda7427cfaa208a11bb690c
Droid664 <iskandarov.murat@gmail.com>
2024-04-12 23:29:47 +0500

    Добавил CORS

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+  const PORT = Number(configService.get<string>('PORT'))
 
   app.setGlobalPrefix('/api')
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    origin: configService.get<string>('ORIGIN'),
+  })
 
-  await app.listen(1337)
+  await app.listen(PORT)
 }
 bootstrap()
